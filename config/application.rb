7228a31fc55947bcd1b2c1ef433c57fb1b0919f0
Zach Alewel <zachalewel@gmail.com>
2016-03-24 19:53:22 -0600

    adding config settings

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,10 @@ module Chatapp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.encoding = 'utf-8'
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
 
   end
 end
